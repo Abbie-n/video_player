5c5bb938ae2dc58f24fc5d5520f92a4f06e2e7ca
AbbieN <52113711+Abbie-n@users.noreply.github.com>
2022-10-08 00:51:26 +0100

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const VideoPlayerApp());
+
+class VideoPlayerApp extends StatelessWidget {
+  const VideoPlayerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
